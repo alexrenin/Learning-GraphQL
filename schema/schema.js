70a5f9550659fd669cf1b59d900e55ce210c3d29
Alex <sanya_renin@mail.ru>
2020-02-01 16:32:18 +0300

    type relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,20 @@
 const graphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID,
+	GraphQLInt } = graphql
 
 const movies = [
-	{id: '1', name: 'name 1', genre: 'genre1'},
-	{id: '2', name: 'name 2', genre: 'genre1'},
-	{id: 3, name: 'name 3', genre: 'genre2'},
-	{id: 4, name: 'name 4', genre: 'genre2'},
+	{id: '1', name: 'name 1', genre: 'genre1', directorId: '1', },
+	{id: '2', name: 'name 2', genre: 'genre1', directorId: '2',},
+	{id: 3, name: 'name 3', genre: 'genre2', directorId: '3',},
+	{id: 4, name: 'name 4', genre: 'genre2', directorId: '4',},
+]
+
+const directors = [
+	{ id: '1', name: 'Director 1', age: 51},
+	{ id: '2', name: 'Director 2', age: 52},
+	{ id: '3', name: 'Director 3', age: 53},
+	{ id: '4', name: 'Director 4', age: 54},
 ]
 
 const MovieType = new GraphQLObjectType({
@@ -14,7 +22,22 @@ const MovieType = new GraphQLObjectType({
 	fields: () => ({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
-		genre: { type: GraphQLString }
+		genre: { type: GraphQLString },
+		director: {
+			type: DirectorType,
+			resolve(parent, args) {
+				return directors.find(director => director.id == parent.id)
+			}
+		}
+	})
+})
+
+const DirectorType = new GraphQLObjectType({
+	name: 'Director',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
 	})
 })
 
@@ -26,6 +49,13 @@ const Query = new GraphQLObjectType({
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				return movies.find(movie => movie.id == args.id)
+			}
+		},
+		director: {
+			type: DirectorType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return directors.find(director => director.id == args.id)
 			}
 		}
 	}
