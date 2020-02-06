5c73c154fb7fa92d1862237653b08d12c771155f
Alex <sanya_renin@mail.ru>
2020-02-06 16:39:31 +0300

    #11 connection between front-end and back-end

--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('../schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const {USER_MONGODB, PASSWORD_MONGODB} = require('../config/config')
 
@@ -13,10 +14,14 @@ mongoose.connect(
 	{ useNewUrlParser: true }
 )
 
+app.use(cors())
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }))
+
+
 
 const dbConnection = mongoose.connection
 dbConnection.on('error', err => console.log(`Connection error: ${err}`))
